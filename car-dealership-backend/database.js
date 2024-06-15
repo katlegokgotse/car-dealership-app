e0d1e6eba5065dde1651b0cc9eca949e7cca0596
Katlego Kgotse <katlegokgotse88@gmail.com>
2024-06-15 21:13:57 +0200

    Ijohhh ke mathatha

--- car-dealership-backend/database.js
+++ car-dealership-backend/database.js
@@ -1,13 +1,28 @@
 import mysql,{ createPool } from 'mysql2';
+import dotenv from 'dotenv'
 
+//configure the environment variables
+dotenv.config()
 const connectToDatabase = mysql.createPool({
-    host: process.env.HOST,
-    user: process.env.USER,
-    password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
 }).promise();
 
-function connection(){
+async function getCars(id){
+    const [rows] = await connectToDatabase.query(`
+        SELECT * 
+        from CARS
+        WHERE car_id = ?
+        `, [id]);
+    return rows[0];
+}
+
+const cars = await getCars()
+console.log(cars)
+
+/*function connection(){
     connectToDatabase.connect((error) => {
     if(error){
         console.log("Error connecting to the database");
@@ -17,6 +32,6 @@ function connection(){
         console.log('Connection Established')
     }
 })
-}
-connection()
+}*/
+//connection()
 connectToDatabase.end();
--- car-dealership-backend/index.js
+++ car-dealership-backend/index.js
@@ -1,10 +1,16 @@
-const express = require('express');
-const { connection } = require('./services/database/database');
+import express from 'express';
+import { connection } from './database';
+import mysql2 from mysql2
 
 const app = express()
 const port = 3000;
 app.get('/', (req, res) => {
     res.send("Hello, World!")
+})
+
+app.get("/cars_ad", (req, res)=>{
+    const get_cars = "SELECT * FROM dealershipDB"
+    connection.quer
 })
 
 app.listen(port, function() {
