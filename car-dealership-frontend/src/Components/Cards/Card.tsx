d38a44499e62959854cd253333a0709d5855fad3
Katlego Kgotse <katlegokgotse88@gmail.com>
2024-06-27 10:54:41 +0200

    Setting packages upp

---
+++ car-dealership-frontend/src/Components/Cards/Card.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+type Props = {
+}
+
+const Card = (props: Props) => {
+  return (
+    <div>
+        <div>
+            <img src=''/>
+            <div>
+                <h2>Car.Model</h2>
+                <p>Car.Description</p>
+                <p>Car.State</p>
+                <button>R890K</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Card
