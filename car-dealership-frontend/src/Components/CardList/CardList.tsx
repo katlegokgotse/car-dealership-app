8ac0a650a4d4cb92f4e35f3240b8df661a199b09
Katlego Kgotse <katlegokgotse88@gmail.com>
2024-06-28 13:29:55 +0200

    Fixing the design of the card component

--- car-dealership-frontend/src/Components/CardList/CardList.tsx
+++ car-dealership-frontend/src/Components/CardList/CardList.tsx
@@ -1,16 +1,33 @@
 import React from 'react'
 import Card from '../Cards/Card'
 
-interface Props{
+interface Props{  
 
 }
 
-const CardList = () => {
+const CardList = (props: Props) => {
   return (
     <div>
-        <Card/>
-        <Card/>
-        <Card/>
+        <Card 
+        image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRxLkf2I8NT1UzTbEuXIYxHakacuAjZMuR5kg&s' 
+        model='i30N' 
+        make='Hyunadi' 
+        price={500000}
+         year={2024}
+         state='used'
+         color='black'
+         extras='none'
+         />
+          <Card 
+        image='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnOTxMnYHQntvLmu8L5galSstsSe8rYeLcHA&s' 
+        model='i30N' 
+        make='Hyunadi' 
+        price={625000}
+         year={2023}
+         state='used'
+         color='black'
+         extras='panaromic sun roof'
+         />
     </div>
   )
 }
--- car-dealership-frontend/src/Components/Cards/Card.tsx
+++ car-dealership-frontend/src/Components/Cards/Card.tsx
@@ -4,20 +4,29 @@ interface Props{
     image: string;
     model: string;
     make: string;
-    cost: string;
+    price: number;
+    year: number;
+    state: string;
+    color: string
+    extras: string;
 }
 
 const Card = (props: Props) => {
   return (
-    <div>
-        <div>
-            <img src=''/>
-            <div>
-                <h2>Car.Model</h2>
-                <p>Car.Description</p>
-                <p>Car.State</p>
-                <button>R890K</button>
+    <div className='Card'>
+        <div className='card-contents'>
+            <div className='card-image'>
+                <img src={props.image} />
             </div>
+            <div className='card-text'>
+                <p className='card-money'>R {props.price}</p>
+                <div>
+                <p>{props.state}</p>
+                <p className='card-information'>{props.year} {props.make} {props.model}</p>
+                <p>{props.extras}</p>
+                </div>
+            </div>
+            <button>Find out more</button>
         </div>
     </div>
   )
